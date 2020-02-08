302acbcff37d9a3914cb5352bba3a5af356ccd93
Nozomi Ishii <non.ankt@gmail.com>
2020-02-09 01:16:06 +0900

    studyin restful api

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-const heyYou = require('doing-well');
+const http = require('http');
 
-heyYou.doingWell('nozomi');
+const server = http.createServer((req, res)=>{
+  if(req.url==="/"){
+    res.write('tasty pussy')
+    res.end();
+  }
+
+  if(req.url==="/pussy"){
+    res.write('she needs dick')
+    res.end();
+  }
+})
+
+server.listen('1919');
+console.log('listening on port 1919');
 
