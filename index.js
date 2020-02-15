2fdf34f8fbd51be8088b0b3ccf1c78f05287343a
Nozomi Ishii <non.ankt@gmail.com>
2020-02-16 07:19:34 +0900

    studing about path

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
-  let filePath = path.join(__dirname, 'public', req.url === '/' ? "index.html" : req.url)
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? "index.html" : req.url);
+  console.log(filePath);
+
+  let extname = path.extname(filePath);
+  let contentType = 'text/html';
+
+  res.end();
 });
 
 const PORT = process.env.PORT || 3000;
