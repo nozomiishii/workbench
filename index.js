acc3b97c45ba11e31a39cb60d19f5cb44ec98f8b
Nozomi Ishii <non.ankt@gmail.com>
2020-02-16 07:02:36 +0900

    created public folder

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
-const mongoose = require('mongoose');
-mongoose.set('useUnifiedTopology', true);
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
-  .then(()=> console.log('connecting mongodb...'))
-  .catch(err => console.error('mongodb connection failed..', err));
+const server = http.createServer((req, res)=>{
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? "index.html" : req.url)
+});
 
-const courseSchema = new mongoose.Schema({
-  name: String,
-  author: String,
-  tags: [String],
-  date: {type: Date, default: Date.now},
-  isPublished: Boolean
-})
-
-const Course = mongoose.model('Course', courseSchema);
-
-async function createCourse(){
-  const course = new Course({
-    name: "sex",
-    author: "midori",
-    tags: ["juicy", "white", "dripping"],
-    isPublished: true
-  }) 
-  const result = await course.save();
-  console.log(result);
-}
-
-async function getCourse(){
-  const courses = await Course
-    .find({isPublished: true})
-    .limit(3)
-    .sort({name: 1});
-  console.log(courses);
-}
-
-
-// createCourse();
-getCourse();
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, ()=> console.log(`server running on port${PORT}`));
