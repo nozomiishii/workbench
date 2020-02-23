54de572a5bd0bab733e0b41ad273e3b6d789ba36
Nozomi Ishii <non.ankt@gmail.com>
2020-02-23 16:14:08 +0900

    studying bootstrap

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const ul = document.querySelector('ul');
-ul.addEventListener('click', e =>{
-  if(e.target.tagName === 'LI'){
-    e.target.remove();
-  }
-})
 
-
-const p = document.querySelector('p');
-
-p.addEventListener('copy', e =>{
-  console.log('nice pussy');
-})
