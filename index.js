ecaaff9fc15321a1f971bfc097a50ac36a7846fc
Nozomi Ishii <non.ankt@gmail.com>
2020-02-27 05:20:39 +0900

    creating quiz

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const correctAnsters = ['B', 'B', 'B', 'B']
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
 
+form.addEventListener('submit', e =>{
+  e.preventDefault();
+
+})
