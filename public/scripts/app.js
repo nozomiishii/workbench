8de69a63e8f12fe0bb40446f9ab26a3b1ddf650e
Nozomi Ishii <non.ankt@gmail.com>
2020-03-01 15:21:23 +0900

    react

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,65 +1,9 @@
 "use strict";
 
-console.log("app");
+var fullName = "nozomi ishii";
 
-var book = {
-  title: "sex is not jerkoff",
-  round: 3,
-  subtitle: "lick or lick more",
-  options: ["header", "more header"]
+var getFirstName = function getFirstName(name) {
+  return name.split(" ")[0];
 };
 
-var displayOptions = function displayOptions() {
-  if (book.options) {
-    var options = book.options.map(function (option) {
-      React.createElement(
-        "p",
-        null,
-        option
-      );
-    });
-    console.log(options);
-    return options;
-  }
-};
-
-var template = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
-    null,
-    book.title
-  ),
-  book.subtitle ? React.createElement(
-    "p",
-    null,
-    book.subtitle
-  ) : undefined,
-  displayOptions()
-);
-
-var intro = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
-    null,
-    "nozomi ishii"
-  ),
-  React.createElement(
-    "p",
-    null,
-    "age: 28"
-  ),
-  React.createElement(
-    "p",
-    null,
-    "location: tokyo"
-  )
-);
-
-var appRoot = document.getElementById("app");
-
-ReactDOM.render(template, appRoot);
-// ReactDOM.render(intro, appRoot);
+console.log(getFirstName(fullName));
