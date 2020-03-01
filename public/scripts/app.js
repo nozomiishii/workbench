650c05e293957dd1254b3ef658ecb26fdb14a302
Nozomi Ishii <non.ankt@gmail.com>
2020-03-02 01:07:11 +0900

    arrow function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,24 @@
 "use strict";
 
-var fullName = "nozomi ishii";
+// const multiply = () => {
+//   const newArray = this.numbers.map(number => {
+//     return number * this.multiplyBy;
+//   });
+//   return newArray;
+// };
+var multiply = void 0;
 
-var getFirstName = function getFirstName(name) {
-  return name.split(" ")[0];
+var multiplier = {
+  numbers: [2, 3, 4],
+  multiplyBy: 3,
+  multiply: function multiply() {
+    var _this = this;
+
+    var newArray = this.numbers.map(function (number) {
+      return number * _this.multiplyBy;
+    });
+    return newArray;
+  }
 };
 
-console.log(getFirstName(fullName));
+console.log(multiplier.multiply());
--- src/playground.js
+++ src/playground.js
@@ -1,5 +1,20 @@
-const fullName = "nozomi ishii";
+// const multiply = () => {
+//   const newArray = this.numbers.map(number => {
+//     return number * this.multiplyBy;
+//   });
+//   return newArray;
+// };
+let multiply;
 
-const getFirstName = name => name.split(" ")[0];
+const multiplier = {
+  numbers: [2, 3, 4],
+  multiplyBy: 3,
+  multiply: function() {
+    const newArray = this.numbers.map(number => {
+      return number * this.multiplyBy;
+    });
+    return newArray;
+  }
+};
 
-console.log(getFirstName(fullName));
+console.log(multiplier.multiply());
