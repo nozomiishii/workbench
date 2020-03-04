aaef1e20dab73e6a81a7453fa2fe32192200ee8f
Nozomi Ishii <non.ankt@gmail.com>
2020-03-04 22:28:30 +0900

    hide and show btn

---
+++ react/hideAndShow.js
@@ -0,0 +1,21 @@
+const root = document.getElementById("app");
+
+let visibilty = true;
+
+const toggleVisibilty = () => {
+  console.log(visibilty);
+  visibilty = !visibilty;
+  render();
+};
+
+const render = () => {
+  const template = (
+    <div>
+      <h1>Hide and show</h1>
+      <button onClick={toggleVisibilty}>{visibilty ? "show" : "hide"}</button>
+    </div>
+  );
+  ReactDOM.render(template, root);
+};
+
+render();
