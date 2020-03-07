30b098ba6f77447e7f2e3b3ae6056567c875e6b1
Nozomi Ishii <non.ankt@gmail.com>
2020-03-07 17:28:45 +0900

    react

---
+++ src/components/Action.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Action = props => {
+  return (
+    <div>
+      <button disabled={!props.hasOptions} onClick={props.makeDicision}>
+        What should I do?
+      </button>
+    </div>
+  );
+};
+
+export default Action;
