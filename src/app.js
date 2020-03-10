dca41cc87835e7de75e07930bbd2a6e0381766cf
Nozomi Ishii <non.ankt@gmail.com>
2020-03-11 03:50:01 +0900

    redux

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+
+const store = configureStore();
+store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch(addExpense({ description: "water bill" }));
+store.dispatch(addExpense({ description: "gas bill" }));
+store.dispatch(setTextFilter("bill"));
+store.dispatch(setTextFilter("water"));
 
 ReactDOM.render(<AppRouter />, document.getElementById("app"));
