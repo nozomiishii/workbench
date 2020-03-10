e8864ba8d025360507adfb16b50613f6fbbb8a44
Nozomi Ishii <non.ankt@gmail.com>
2020-03-10 12:47:14 +0900

    redux

---
+++ src/playground/redux-101.js
@@ -0,0 +1,34 @@
+import { createStore } from "redux";
+
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      };
+    case "DECREMENT":
+      return {
+        count: state.count - 1
+      };
+    case "RESET":
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
+});
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch({
+  type: "INCREMENT"
+});
+
+store.dispatch({
+  type: "RESET"
+});
+
+store.dispatch({
+  type: "DECREMENT"
+});
