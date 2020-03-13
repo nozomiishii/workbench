896a6f89b85db3530e5ff7d0dab4a95244a0affb
Nozomi Ishii <non.ankt@gmail.com>
2020-03-14 00:52:51 +0900

    redux

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
+import ExpenseForm from "./ExpenseForm";
 
 const EditExpensePage = props => {
   console.log(props);
-  return <div>You are edittin page no.{props.match.params.id}</div>;
+  return (
+    <div>
+      <ExpenseForm
+        expense={props.expense}
+        onSubmit={expense => console.log("update", expense)}
+      />
+    </div>
+  );
 };
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find(
+      expense => expense.id === props.match.params.id
+    )
+  };
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
