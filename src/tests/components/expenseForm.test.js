49581a54e5ea8aa2f1068d3724accc4363c4f456
Nozomi Ishii <non.ankt@gmail.com>
2020-03-15 04:23:00 +0900

    test

--- src/tests/components/expenseForm.test.js
+++ src/tests/components/expenseForm.test.js
@@ -67,3 +67,20 @@ test("should set amount on input change", () => {
     });
   expect(wrapper.state("amount")).toBe("");
 });
+
+test("should call onSubmit prop for vaild from submisstion", () => {
+  const onSubmitSpy = jest.fn();
+  const wrapper = shallow(
+    <ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy} />
+  );
+  wrapper.find("form").simulate("submit", {
+    preventDefault: () => {}
+  });
+  expect(wrapper.state("error")).toBe("");
+  expect(onSubmitSpy).toHaveBeenLastCalledWith({
+    description: expenses[0].description,
+    amount: expenses[0].amount,
+    note: expenses[0].note,
+    createdAt: expenses[0].createdAt
+  });
+});
