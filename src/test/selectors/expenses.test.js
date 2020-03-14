11730fe907d34561468b01530dc7b54e4577978b
Nozomi Ishii <non.ankt@gmail.com>
2020-03-14 16:52:57 +0900

    test

---
+++ src/test/selectors/expenses.test.js
@@ -0,0 +1,37 @@
+import moment from "moment";
+import selectExpenses from "../../selectors/expenses";
+
+const expenses = [
+  {
+    id: 1,
+    description: "gum",
+    note: "",
+    amount: 20,
+    createdAt: 0
+  },
+  {
+    id: 2,
+    description: "rent",
+    note: "",
+    amount: 104500,
+    createdAt: -1000
+  },
+  {
+    id: 3,
+    description: "creadit card",
+    note: "",
+    amount: 4500,
+    createdAt: 1000
+  }
+];
+
+test("should filter by text value", () => {
+  const filters = {
+    text: "e",
+    sortBy: "date",
+    startDate: undefined,
+    endDate: undefined
+  };
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[2], expenses[1]]);
+});
