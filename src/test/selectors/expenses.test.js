29e27eae0840b70947d46f7de062b66b3fab8dfb
Nozomi Ishii <non.ankt@gmail.com>
2020-03-14 17:30:31 +0900

    test

--- src/test/selectors/expenses.test.js
+++ src/test/selectors/expenses.test.js
@@ -14,14 +14,18 @@ const expenses = [
     description: "rent",
     note: "",
     amount: 104500,
-    createdAt: -1000
+    createdAt: moment(0)
+      .subtract(4, "days")
+      .valueOf()
   },
   {
     id: 3,
     description: "creadit card",
     note: "",
     amount: 4500,
-    createdAt: 1000
+    createdAt: moment(0)
+      .add(4, "days")
+      .valueOf()
   }
 ];
 
@@ -34,4 +38,48 @@ test("should filter by text value", () => {
   };
   const result = selectExpenses(expenses, filters);
   expect(result).toEqual([expenses[2], expenses[1]]);
+});
+
+test("should filter by startDate", () => {
+  const filters = {
+    text: "",
+    sortBy: "date",
+    startDate: moment(0),
+    endDate: undefined
+  };
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[2], expenses[0]]);
+});
+
+test("should filter by endDate", () => {
+  const filters = {
+    text: "",
+    sortBy: "date",
+    startDate: undefined,
+    endDate: moment(0).add(2, "days")
+  };
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[0], expenses[1]]);
+});
+
+test("should filter by date", () => {
+  const filters = {
+    text: "",
+    sortBy: "date",
+    startDate: undefined,
+    endDate: undefined
+  };
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[2], expenses[0], expenses[1]]);
+});
+
+test("should filter by amount", () => {
+  const filters = {
+    text: "",
+    sortBy: "amount",
+    startDate: undefined,
+    endDate: undefined
+  };
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[1], expenses[2], expenses[0]]);
 });
