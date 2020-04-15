4fdde42a3fdf70f94e940b028e14961a650997df
Nozomi Ishii <non.ankt@gmail.com>
2020-04-16 05:01:33 +0900

    db

--- mongodb.js
+++ mongodb.js
@@ -12,9 +12,43 @@ MongoClient.connect(
       return console.log("unable to connct ro db");
     }
     const db = client.db(databaseName);
-    db.collection("users").insertOne({
-      name: "nozomi",
-      age: 28,
-    });
+    db.collection("users").insertMany(
+      [
+        {
+          name: "nozomi",
+          age: 28,
+        },
+        {
+          name: "midori",
+          age: 27,
+        },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("unable to insert taskes");
+        }
+        console.log(result);
+        console.log(result.ops);
+      }
+    );
+
+    db.collection("tasks").insertMany(
+      [
+        {
+          description: "clean pc",
+          completed: true,
+        },
+        {
+          description: "lick pussy",
+          completed: false,
+        },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log(result.ops);
+      }
+    );
   }
 );
