6511dfcaa9315ed45716ef22b70eddbebb1ae836
Nozomi Ishii <non.ankt@gmail.com>
2020-04-18 01:42:57 +0900

    mongodb

---
+++ mongodb.js
@@ -0,0 +1,21 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (error, client) => {
+    if (error) {
+      return console.log("unable to connect to database");
+    }
+
+    const db = client.db(databaseName);
+    db.collection("users").insertOne({
+      name: "nozomi",
+      age: 28,
+    });
+  }
+);
