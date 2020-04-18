8f1037048babdda4332419cdbc5dc7599da62cc1
Nozomi Ishii <non.ankt@gmail.com>
2020-04-19 02:37:53 +0900

    task app

--- src/index.js
+++ src/index.js
@@ -21,6 +21,27 @@ app.post("/users", (req, res) => {
     });
 });
 
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((err) => res.status(500).send());
+});
+
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch((error) => res.status(500).send());
+});
+
 app.post("/tasks", (req, res) => {
   const task = new Task(req.body);
 
@@ -30,4 +51,25 @@ app.post("/tasks", (req, res) => {
     .catch((error) => res.status(400).send(error));
 });
 
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((err) => res.status(500).send());
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+      res.send(task);
+    })
+    .catch((error) => {
+      res.status(500).send();
+    });
+});
 app.listen(port, () => console.log(`listening port ${port}`));
