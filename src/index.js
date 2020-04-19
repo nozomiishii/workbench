d9a88f28c4c7d82fd9da6ce6ab9434af3cbfbc6e
Nozomi Ishii <non.ankt@gmail.com>
2020-04-19 18:30:46 +0900

    patch

--- src/index.js
+++ src/index.js
@@ -41,6 +41,33 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
+app.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "invalid updates" });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
 
@@ -74,4 +101,4 @@ app.get("/tasks/:id", async (req, res) => {
     res.status(500).send();
   }
 });
-app.listen(port, () => console.log(`listening port ${port}`));
+app.listen(port, () => console.log(`server started on port ${port}`));
