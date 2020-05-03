6ba67d541414495d4cc3d59eae4e34928b02dc0d
Nozomi Ishii <non.ankt@gmail.com>
2020-05-03 22:39:55 +0900

    react styling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => (
-  <div>
-    <h1>React App</h1>
-  </div>
-);
+const App = () => {
+  const name = "nozomi";
+  const date = new Date();
+
+  let n = 100;
+  const customStyle = {
+    width: n + "px",
+    height: "100px",
+  };
+
+  return (
+    <div>
+      <h1>Hello {name}</h1>
+      <p>©{date.getFullYear()}</p>
+      <div>
+        <img
+          style={customStyle}
+          src="https://picsum.photos/200"
+          alt=""
+          crossOrigin="anonymous"
+        />
+      </div>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
