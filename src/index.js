4807b066e0cbbf5df1500c0081556178b0e80916
Nozomi Ishii <non.ankt@gmail.com>
2020-05-03 20:43:16 +0900

    create react app

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
-const express = require("express");
-const app = express();
-require("./data/mongoose");
-const usersRouter = require("./routers/users");
-const tasksRouter = require("./routers/tasks");
-const port = process.env.PORT || 3000;
+import React from "react";
+import ReactDOM from "react-dom";
 
-app.use(express.json());
-app.use(usersRouter);
-app.use(tasksRouter);
+const App = () => (
+  <div>
+    <h1>React App</h1>
+  </div>
+);
 
-app.listen(port, () => console.log(`server started on port ${port}`));
-
-// const Task = require("./models/task");
-// const User = require("./models/user");
-
-// const main = async () => {
-//   const task = await Task.findById("5ea35445c9ea72687a31b62b");
-//   console.log(task.owner);
-//   const user = await User.findById(task.owner);
-//   await user.populate("tasks").execPopulate();
-//   console.log(user.tasks);
-// };
-
-// main();
+ReactDOM.render(<App />, document.getElementById("root"));
