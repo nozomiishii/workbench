cc7161f0e62abe40032d581133406779611ac20c
Nozomi Ishii <non.ankt@gmail.com>
2020-05-04 01:02:12 +0900

    react practice

--- src/index.js
+++ src/index.js
@@ -1,30 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const App = () => {
-  const name = "nozomi";
-  const date = new Date();
-
-  let n = 100;
-  const customStyle = {
-    width: n + "px",
-    height: "100px",
-  };
-
-  return (
-    <div>
-      <h1>Hello {name}</h1>
-      <p>©{date.getFullYear()}</p>
-      <div>
-        <img
-          style={customStyle}
-          src="https://picsum.photos/200"
-          alt=""
-          crossOrigin="anonymous"
-        />
-      </div>
-    </div>
-  );
-};
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
