57e18767f74f85fe113866911930d060b7ce9fd7
Nozomi Ishii <non.ankt@gmail.com>
2020-05-04 18:35:53 +0900

    custom link

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link as ReactRouterDomLink, useLocation } from "react-router-dom";
 
 const HeaderWrapper = styled.header`
   height: 60px;
@@ -13,11 +13,44 @@ const HeaderWrapper = styled.header`
   background: #eee;
 `;
 
+const Menu = styled.nav`
+  display: flex;
+  position: relative;
+  width: initial;
+  border-bottom: none;
+  margin: auto 0 auto auto;
+  font-family: "Open Sans";
+  background: none;
+  left: initial;
+  top: initial;
+`;
+
+const Link = ({ isActive, children, ...props }) => (
+  <ReactRouterDomLink {...props}>{children}</ReactRouterDomLink>
+);
+
+const StyledLink = styled(Link)`
+  padding: 4px 8px;
+  display: block;
+  text-align: center;
+  box-sizing: border-box;
+  margin: auto 0;
+  font-weight: ${({ isActive }) => (isActive ? "bold" : "normal")};
+  color: black;
+`;
+
 export const Header = () => {
+  const { pathname } = useLocation();
   return (
     <HeaderWrapper>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
+      <Menu>
+        <StyledLink to="/" isActive={pathname === "/"}>
+          Home
+        </StyledLink>
+        <StyledLink to="/login" isActive={pathname === "/login"}>
+          Login
+        </StyledLink>
+      </Menu>
     </HeaderWrapper>
   );
 };
