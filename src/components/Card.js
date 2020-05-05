995e067497180a2c956f1070244ab28a3615ada4
Nozomi Ishii <non.ankt@gmail.com>
2020-05-06 04:24:56 +0900

    refactoring

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,32 +7,12 @@ const Wrapper = styled.div`
   position: relative;
   padding: 25px 15px;
   background-color: #81ecec;
-  margin: 50px 0;
+  margin: 50px auto;
   height: 220px;
   box-shadow: 0 2px 5px #ccc;
   text-align: left;
   max-width: 400px;
   min-width: 400px;
-  margin: 0 auto;
-  padding: 25px;
-  .top {
-    border-radius: 25px 25px 0 0;
-    height: 100px;
-    width: 100%;
-    background-color: #00cec9;
-    position: absolute;
-    top: 0;
-    left: 0;
-    display: flex;
-  }
-
-  .name {
-    font-size: 2em;
-    color: #2d3436;
-    display: flex;
-    flex: 1;
-    margin: 10% 20px 0;
-  }
 
   .bottom {
     margin-top: 120px;
@@ -43,16 +23,34 @@ const Wrapper = styled.div`
   }
 `;
 
+const Top = styled.div`
+  border-radius: 25px 25px 0 0;
+  height: 100px;
+  width: 100%;
+  background-color: #00cec9;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  h2 {
+    font-size: 2em;
+    color: #2d3436;
+    display: flex;
+    flex: 1;
+    margin: 10% 20px 0;
+  }
+`;
+
 export const Card = ({ name, imgURL, phone, email }) => {
   return (
     <Wrapper>
-      <div className="top">
-        <h2 className="name">{name}</h2>
+      <Top>
+        <h2>{name}</h2>
         <Avatar imgURL={imgURL} />
-      </div>
+      </Top>
       <div className="bottom">
-        <p className="info">{phone}</p>
-        <p className="info">{email}</p>
+        <p>{phone}</p>
+        <p>{email}</p>
       </div>
     </Wrapper>
   );
