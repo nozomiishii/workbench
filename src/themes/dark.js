c38e649630eece75d887b0498fe819f4c4fac1dc
Nozomi Ishii <non.ankt@gmail.com>
2020-05-05 19:22:32 +0900

    delete;

--- src/themes/dark.js
+++
@@ -1,9 +0,0 @@
-const theme = {
-  id: "dark",
-  primary: "black",
-  secondary: "#f8049c",
-  backGroundColor: "black",
-  fontColor: "white",
-};
-
-export default theme;
--- src/themes/light.js
+++
@@ -1,9 +0,0 @@
-const theme = {
-  id: "light",
-  primary: "#f8049c",
-  secondary: "#330867",
-  backGroundColor: "white",
-  fontColor: "black",
-};
-
-export default theme;
