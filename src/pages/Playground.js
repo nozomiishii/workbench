3cbc695b0fd90a9089abe6ff66fc2b84abf26dc3
Nozomi Ishii <non.ankt@gmail.com>
2020-05-08 03:05:53 +0900

    timer

--- src/pages/Playground.js
+++ src/pages/Playground.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { PageLayout } from "../components";
-import { Counter, CurrentTimer } from "../templates";
+import { Counter } from "../templates";
 
 export const Playground = () => {
   return (
     <PageLayout>
       <h1>Playground</h1>
-      <CurrentTimer />
       <Counter />
     </PageLayout>
   );
