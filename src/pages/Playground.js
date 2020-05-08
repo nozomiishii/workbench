8b8a226852cdb0e2c3f3afd888f1c90a5ddd9f51
Nozomi Ishii <non.ankt@gmail.com>
2020-05-08 22:08:46 +0900

    created memon

--- src/pages/Playground.js
+++ src/pages/Playground.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { PageLayout } from "../components";
-import { Counter } from "../templates";
+import { Counter, TodoList } from "../templates";
 
 export const Playground = () => {
   return (
     <PageLayout>
       <h1>Playground</h1>
+      <TodoList />
       <Counter />
     </PageLayout>
   );
--- src/templates/index.js
+++ src/templates/index.js
@@ -1,2 +1,2 @@
 export * from "./Counter";
-export * from "./CurrentTimer";
+export * from "./TodoList";
