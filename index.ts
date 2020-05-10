d4a59bdb4666c80350e066139706390bd8144e9c
Nozomi Ishii <non.ankt@gmail.com>
2020-05-10 19:23:30 +0900

    ts

--- index.ts
+++ index.ts
@@ -9,13 +9,15 @@ interface Todo {
 }
 
 axios.get(url).then((response) => {
-  const todo = response.data as Todo;
-  const id = todo.id;
-  const title = todo.title;
-  const completed = todo.completed;
+  const { id, title, completed } = response.data as Todo;
+
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
     The Todo with ID: ${id}
     has a title of: ${title}
     Is it finished?: ${completed}
   `);
-});
+};
