757dc76ac5f94356b3908ae153aeea696948e067
Nozomi Ishii <non.ankt@gmail.com>
2020-05-11 04:02:39 +0900

    ts

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 class Vehicle {
+  constructor(public color: string) {}
   drive(): void {
     console.log("chugga chugga");
   }
 }
 
-const vehicle = new Vehicle();
-vehicle.drive();
+const vehicle = new Vehicle("red");
+console.log(vehicle.color);
+
+// class Car extends Vehicle {}
+
+// const car = new Car();
+// car.drive();
