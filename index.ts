21b7b0ef1cf520d478e0353173ed50b3b9a7a6d8
Nozomi Ishii <non.ankt@gmail.com>
2020-05-10 18:30:57 +0900

    ts

--- index.ts
+++ index.ts
@@ -2,4 +2,14 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
-axios.get(url).then((response) => console.log(response.data));
+axios.get(url).then((response) => {
+  const todo = response.data;
+  const id = todo.id;
+  const title = todo.title;
+  const finished = todo.finished;
+  console.log(`
+    The Todo with ID: ${id}
+    has a title of: ${title}
+    Is it finished?: ${finished}
+  `);
+});
