bddcba5de08e927d66a82d0ca65e6210b02d1b7d
Nozomi Ishii <non.ankt@gmail.com>
2020-05-12 06:44:35 +0900

    ts

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,13 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var sorter_1 = require("./sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
-var CharactersCollection_1 = require("./CharactersCollection");
 var numbersCollection = new NumbersCollection_1.NumbersCollection([68, 3, -5, 0]);
+numbersCollection.sort();
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 console.log(numbersCollection.data);
-var charactersCollection = new CharactersCollection_1.CharactersCollection("fkeaRhuAFri");
-var sorter = new sorter_1.Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection.data);
+// const charactersCollection = new CharactersCollection("fkeaRhuAFri");
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,14 +1,14 @@
-import { Sorter } from "./sorter";
+import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 
 const numbersCollection = new NumbersCollection([68, 3, -5, 0]);
-
+numbersCollection.sort();
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection("fkeaRhuAFri");
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection.data);
+// const charactersCollection = new CharactersCollection("fkeaRhuAFri");
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
