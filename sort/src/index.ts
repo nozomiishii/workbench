1c2a577adb35e23828059907f2dae69c8aca9831
Nozomi Ishii <non.ankt@gmail.com>
2020-05-12 02:47:11 +0900

    sort

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from "./sorter";
 import { NumbersCollection } from "./NumbersCollection";
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const numbersCollection = new NumbersCollection([68, 3, -5, 0]);
 
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
