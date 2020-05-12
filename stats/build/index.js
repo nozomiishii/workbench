d8c4e8a3e8c79d0c8c0caeaa751e4c99f36ad3a5
Nozomi Ishii <non.ankt@gmail.com>
2020-05-12 16:53:00 +0900

    stats app

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,2 +1,19 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var matchs = fs_1.default
+    .readFileSync("football.csv", {
+    encoding: "utf-8",
+})
+    .split("\n")
+    .map(function (row) {
+    return row.split(",");
+});
+console.log(matchs);
+var manUnitedWins = 0;
+for (var _i = 0, matchs_1 = matchs; _i < matchs_1.length; _i++) {
+    var match = matchs_1[_i];
+}
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,17 @@
 import fs from "fs";
+
+const matchs = fs
+  .readFileSync("football.csv", {
+    encoding: "utf-8",
+  })
+  .split("\n")
+  .map((row: string): string[] => {
+    return row.split(",");
+  });
+
+console.log(matchs);
+
+let manUnitedWins = 0;
+
+for (let match of matchs) {
+}
