5015cc9db215ce55089978eea1adbfc94eadb1df
Nozomi Ishii <non.ankt@gmail.com>
2020-05-12 19:46:19 +0900

    enum

--- stats/build/index.js
+++ stats/build/index.js
@@ -13,7 +13,20 @@ var matchs = fs_1.default
     return row.split(",");
 });
 console.log(matchs);
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
 var manUnitedWins = 0;
 for (var _i = 0, matchs_1 = matchs; _i < matchs_1.length; _i++) {
     var match = matchs_1[_i];
+    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
 }
+console.log("Man United won " + manUnitedWins + " games");
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,17 +1,22 @@
-import fs from "fs";
+import { CsvFileReader } from "./CsvFileReader";
 
-const matchs = fs
-  .readFileSync("football.csv", {
-    encoding: "utf-8",
-  })
-  .split("\n")
-  .map((row: string): string[] => {
-    return row.split(",");
-  });
+const reader = new CsvFileReader("football.csv");
+reader.read();
 
-console.log(matchs);
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
 
 let manUnitedWins = 0;
 
-for (let match of matchs) {
+for (let match of reader.data) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
 }
+
+console.log(`Man United won ${manUnitedWins} games`);
