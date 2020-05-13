072cac0a9b47d560984fb247aa70c9297473aa15
Nozomi Ishii <non.ankt@gmail.com>
2020-05-13 22:09:49 +0900

    class

--- net-ninja/public/index.js
+++ net-ninja/public/index.js
@@ -9,18 +9,51 @@
 //     console.log("the user logged out");
 //   },
 // };
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // console.log(userOne.email, userOne.username);
 // userOne.login();
 var User = /** @class */ (function () {
     function User(username, email) {
         this.username = username;
         this.email = email;
+        this.score = 0;
     }
     User.prototype.login = function () {
         console.log(this.username + " just logged in");
+        return this;
+    };
+    User.prototype.logout = function () {
+        console.log(this.username + " just logged out");
+        return this;
+    };
+    User.prototype.incScore = function () {
+        this.score += 1;
+        console.log(this.username + " has a score of " + this.score);
+        return this;
     };
     return User;
 }());
+var Admin = /** @class */ (function (_super) {
+    __extends(Admin, _super);
+    function Admin() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Admin;
+}(User));
 var userOne = new User("mario", "mario@gmail");
 var userTwo = new User("chun-li", "chun-li@gmail");
-console.log(userOne, userTwo);
+var userThree = new Admin("shaun", "shuan@netninja.com");
+userOne.login().logout().incScore().incScore();
+console.log(userOne, userTwo, userThree);
--- net-ninja/src/index.ts
+++ net-ninja/src/index.ts
@@ -15,16 +15,34 @@
 class User {
   username: string;
   email: string;
+  score: number;
   constructor(username: string, email: string) {
     this.username = username;
     this.email = email;
+    this.score = 0;
   }
   login() {
     console.log(`${this.username} just logged in`);
+    return this;
+  }
+  logout() {
+    console.log(`${this.username} just logged out`);
+    return this;
+  }
+  incScore() {
+    this.score += 1;
+    console.log(`${this.username} has a score of ${this.score}`);
+    return this;
   }
 }
+
+class Admin extends User {}
 
 const userOne = new User("mario", "mario@gmail");
 const userTwo = new User("chun-li", "chun-li@gmail");
 
-console.log(userOne, userTwo);
+const userThree = new Admin("shaun", "shuan@netninja.com");
+
+userOne.login().logout().incScore().incScore();
+
+console.log(userOne, userTwo, userThree);
