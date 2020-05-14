20479c284d3542ccd648b990e68bda8912b3f1d6
Nozomi Ishii <non.ankt@gmail.com>
2020-05-15 03:13:29 +0900

    ts and react

--- harry-wolff/src/App.tsx
+++ harry-wolff/src/App.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./Accordion";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Accordion preview="Click me for fun!">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident
+          deleniti molestiae, quibusdam ullam necessitatibus nisi aperiam
+          molestias perspiciatis rerum vel laudantium vero eius sed. Facilis
+          magni sed esse possimus dolores.
+        </Accordion>
       </header>
     </div>
   );
