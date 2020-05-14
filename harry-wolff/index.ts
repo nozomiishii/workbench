625113a89fe5d7dc22021b2d255c70bb26b1cd10
Nozomi Ishii <non.ankt@gmail.com>
2020-05-14 21:27:32 +0900

    class ts

--- harry-wolff/index.ts
+++ harry-wolff/index.ts
@@ -1,25 +1,30 @@
-interface Person {
+interface GreetingOptions {
+  log?: boolean;
   name: string;
-  teacher: boolean;
-  hangry?: boolean;
-}
-let person: Person = {
-  name: "Harry",
-  teacher: true,
-};
-
-let person2: Person = {
-  name: "Harry",
-  teacher: true,
-  hangry: false,
-};
-
-function greeting(name: string) {
-  console.log(`Hello ${name}`);
+  parting?: boolean;
 }
 
-greeting("youteqfq");
+class Greeter {
+  log?: boolean;
+  constructor(log: boolean) {
+    this.log = log;
+  }
 
-let str = "hello 2 u";
+  greeting(options: GreetingOptions): string {
+    if (options.log) {
+      console.log(name);
+    }
+    return `Hello to ${name}`;
+  }
+}
 
-let age = 21;
+function greeting(options: GreetingOptions): string {
+  if (options.log) {
+    console.log(name);
+  }
+  return `Hello to ${name}`;
+}
+
+let opts = { name: "nozomi", log: true };
+
+greeting(opts);
