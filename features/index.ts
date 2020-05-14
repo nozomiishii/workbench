4b9e40c5ec103059e15427bfafd1cefe0d6ae850
Nozomi Ishii <non.ankt@gmail.com>
2020-05-14 17:27:15 +0900

    generics

--- features/index.ts
+++ features/index.ts
@@ -39,3 +39,25 @@ function print<T>(arr: T[]): void {
     console.log(arr[i]);
   }
 }
+
+class Car {
+  print() {
+    console.log("I am a car");
+  }
+}
+
+class House {
+  print() {
+    console.log("I am a house");
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
