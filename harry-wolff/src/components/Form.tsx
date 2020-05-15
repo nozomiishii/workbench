f5729318a4e9adaa14ec8cd514afd7c410ac4852
Nozomi Ishii <non.ankt@gmail.com>
2020-05-16 07:21:21 +0900

    useReduser

--- harry-wolff/src/components/Form.tsx
+++ harry-wolff/src/components/Form.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const StyledForm = styled.form`
   margin: 0 auto;
   width: 60%;
-  border: 1px solid red;
+  border: 1px solid black;
   display: flex;
   flex-direction: column;
   padding: 5vw;
@@ -15,14 +15,27 @@ const StyledForm = styled.form`
 `;
 
 export const Form = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+    alert("todo");
+  };
   return (
-    <StyledForm>
+    <StyledForm onSubmit={onSubmit}>
       <p>Please Login</p>
-      <input type="text" placeholder="username" />
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.currentTarget.value)}
+      />
       <input
         type="password"
         placeholder="password"
         autoComplete="new-password"
+        value={password}
+        onChange={(e) => setPassword(e.currentTarget.value)}
       />
       <button>Login</button>
     </StyledForm>
