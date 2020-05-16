c328a5b93200f349ba15e0405f93eaad9289997c
Nozomi Ishii <non.ankt@gmail.com>
2020-05-16 16:58:00 +0900

    type aliases

--- max/src/index.ts
+++ max/src/index.ts
@@ -1,13 +1,28 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-} = {
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHER,
+}
+
+const person = {
   name: "nozomi",
   age: 29,
   hobbies: ["dance", "code"],
+  role: Role.ADMIN,
 };
 
-for (const hobby of person.hobbies) {
-  console.log(hobby.toLowerCase());
+// for (const hobby of person.hobbies) {
+//   console.log(hobby.toLowerCase());
+// }
+
+type Combinable = number | string;
+
+function combine(input1: Combinable, input2: Combinable) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
