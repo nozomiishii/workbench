c4ab1bb4c362fea4b14c76c2dadcd5cf0b334e3e
Nozomi Ishii <non.ankt@gmail.com>
2020-05-18 03:42:15 +0900

    generic

--- max/src/index.ts
+++ max/src/index.ts
@@ -1,49 +1,22 @@
-//
-
-class Car {
-  drive() {
-    console.log("Driving....");
-  }
+interface lengthy {
+  length: number;
 }
 
-class Truck {
-  drive() {
-    console.log("Driving a track...");
+function countAndDescribe<T extends lengthy>(element: T) {
+  let descriptionText = "got no value";
+  if (element.length === 1) {
+    descriptionText = "got 1 element.";
+  } else if (element.length > 1) {
+    descriptionText = "got " + element.length + " elements";
   }
-  loadCargo(amount: number) {
-    console.log("Loading cargo..." + amount);
-  }
+  return [element, descriptionText];
 }
 
-type Vehicle = Car | Truck;
+function extractAncConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return `Value: ${obj[key]}`;
+}
 
-const v1 = new Truck();
-
-const useVehicle = (vehicle: Vehicle) => {
-  if (vehicle instanceof Truck) {
-    vehicle.loadCargo(1000);
-  }
-};
-
-useVehicle(v1);
-
-const button = document.querySelector("button");
-
-// const input = <HTMLInputElement>document.getElementById("user-input")!;
-const input = document.getElementById("user-input")! as HTMLInputElement;
-
-input.value = "hey";
-
-const fetchedUserData = {
-  id: "u1",
-  name: "nozomi",
-  job: { title: "ceo", description: "my company" },
-};
-
-console.log(fetchedUserData?.job?.title);
-
-const nozomi = {
-  age: 29,
-};
-
-console.log(nozomi);
+extractAncConvert({ name: "nozomi" }, "name");
