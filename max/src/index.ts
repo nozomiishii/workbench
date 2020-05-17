f2df218149c6de1993f0bcbba9b9ed84a5905bed
Nozomi Ishii <non.ankt@gmail.com>
2020-05-18 00:31:34 +0900

    type

--- max/src/index.ts
+++ max/src/index.ts
@@ -20,9 +20,30 @@ type Vehicle = Car | Truck;
 const v1 = new Truck();
 
 const useVehicle = (vehicle: Vehicle) => {
-  if ("loadCargo" in vehicle) {
+  if (vehicle instanceof Truck) {
     vehicle.loadCargo(1000);
   }
 };
 
 useVehicle(v1);
+
+const button = document.querySelector("button");
+
+// const input = <HTMLInputElement>document.getElementById("user-input")!;
+const input = document.getElementById("user-input")! as HTMLInputElement;
+
+input.value = "hey";
+
+const fetchedUserData = {
+  id: "u1",
+  name: "nozomi",
+  job: { title: "ceo", description: "my company" },
+};
+
+console.log(fetchedUserData?.job?.title);
+
+const nozomi = {
+  age: 29,
+};
+
+console.log(nozomi);
