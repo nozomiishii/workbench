773eec212ad58602000b4b71a8be7398da5998a5
Nozomi Ishii <non.ankt@gmail.com>
2020-05-19 05:59:11 +0900

    auto bind

--- max/src/index.ts
+++ max/src/index.ts
@@ -1,94 +1,118 @@
-function Logger(logString: string) {
-  return function (target: object) {
-    console.log(logString);
-    console.log(target);
-  };
+// function Logger(logString: string) {
+//   return function (target: object) {
+//     console.log(logString);
+//     console.log(target);
+//   };
+// }
+
+// function WithTemplate(template: string, hookId: string) {
+//   console.log("outer template");
+//   return function <T extends { new (...args: any[]): { name: string } }>(
+//     target: T
+//   ) {
+//     return class extends target {
+//       constructor(...args: any[]) {
+//         super();
+//         console.log("inner template");
+//         const hookEl = document.getElementById(hookId);
+//         if (hookEl) {
+//           hookEl.innerHTML = template;
+//           hookEl.querySelector("h1")!.textContent = this.name;
+//         }
+//       }
+//     };
+//   };
+// }
+
+// @Logger("LOGGING - Person")
+// @WithTemplate("<h1>My Person Object</h1>", "app")
+// class Person {
+//   name: string;
+//   constructor() {
+//     this.name = "nozomi";
+//   }
+// }
+
+// const nozomi = new Person();
+
+// console.log(nozomi);
+
+function Log(target: object, key: string | Symbol) {
+  console.log("Property decorator");
+  console.log(target, key);
 }
 
-function WithTemplate(template: string, hookId: string) {
-  console.log("outer template");
-  return function <T extends { new (...args: any[]): { name: string } }>(
-    target: T
-  ) {
-    return class extends target {
-      constructor(...args: any[]) {
-        super();
-        console.log("inner template");
-        const hookEl = document.getElementById(hookId);
-        if (hookEl) {
-          hookEl.innerHTML = template;
-          hookEl.querySelector("h1")!.textContent = this.name;
-        }
-      }
-    };
-  };
+function Log2(target: object, key: string, desc: PropertyDescriptor) {
+  console.log("accessor decorator");
+  console.log(target);
+  console.log(key);
+  console.log(desc);
 }
 
-@Logger("LOGGING - Person")
-@WithTemplate("<h1>My Person Object</h1>", "app")
-class Person {
-  name: string;
-  constructor() {
-    this.name = "nozomi";
+function Log3(target: object, key: string, desc: PropertyDescriptor) {
+  console.log("Method decorator");
+  console.log(target);
+  console.log(key);
+  console.log(desc);
+}
+
+function Log4(target: object, key: string, position: number) {
+  console.log("Paramater decorator");
+  console.log(target);
+  console.log(key);
+  console.log(position);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive!");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
   }
 }
 
-const nozomi = new Person();
+const book1 = new Product("book1", 50);
+const book2 = new Product("book2", 100);
 
-console.log(nozomi);
+function Autobind(target: object, key: string, desc: PropertyDescriptor) {
+  const targetMethod = desc.value;
+  const newMethod: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const fn = targetMethod.bind(this);
+      return fn;
+    },
+  };
+  return newMethod;
+}
 
-//----
+class Printer {
+  message = "This works!";
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
 
-// function Log(target: object, key: string | Symbol) {
-//   console.log("Property decorator");
-//   console.log(target, key);
-// }
+const p = new Printer();
 
-// function Log2(target: object, key: string, desc: PropertyDescriptor) {
-//   console.log("accessor decorator");
-//   console.log(target);
-//   console.log(key);
-//   console.log(desc);
-// }
-
-// function Log3(target: object, key: string, desc: PropertyDescriptor) {
-//   console.log("Method decorator");
-//   console.log(target);
-//   console.log(key);
-//   console.log(desc);
-// }
-
-// function Log4(target: object, key: string, position: number) {
-//   console.log("Paramater decorator");
-//   console.log(target);
-//   console.log(key);
-//   console.log(position);
-// }
-
-// class Product {
-//   @Log
-//   title: string;
-//   private _price: number;
-
-//   @Log2
-//   set price(val: number) {
-//     if (val > 0) {
-//       this._price = val;
-//     } else {
-//       throw new Error("Invalid price - should be positive!");
-//     }
-//   }
-
-//   constructor(t: string, p: number) {
-//     this.title = t;
-//     this._price = p;
-//   }
-
-//   @Log3
-//   getPriceWithTax(@Log4 tax: number) {
-//     return this._price * (1 + tax);
-//   }
-// }
-
-// const book1 = new Product("book1", 50);
-// const book2 = new Product("book2", 10 0);
+const button = document.querySelector("button") as HTMLButtonElement;
+button.addEventListener("click", p.showMessage);
