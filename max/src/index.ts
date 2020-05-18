3ae46d93abec4e568c796429ced0fc775c7d679b
Nozomi Ishii <non.ankt@gmail.com>
2020-05-19 04:26:58 +0900

    decorator

--- max/src/index.ts
+++ max/src/index.ts
@@ -7,14 +7,20 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
   console.log("outer template");
-  return function (target: any) {
-    console.log("inner template");
-    const hookEl = document.getElementById(hookId);
-    const p = new target();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector("h1")!.textContent = p.name;
-    }
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    target: T
+  ) {
+    return class extends target {
+      constructor(...args: any[]) {
+        super();
+        console.log("inner template");
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h1")!.textContent = this.name;
+        }
+      }
+    };
   };
 }
 
@@ -30,3 +36,59 @@ class Person {
 const nozomi = new Person();
 
 console.log(nozomi);
+
+//----
+
+// function Log(target: object, key: string | Symbol) {
+//   console.log("Property decorator");
+//   console.log(target, key);
+// }
+
+// function Log2(target: object, key: string, desc: PropertyDescriptor) {
+//   console.log("accessor decorator");
+//   console.log(target);
+//   console.log(key);
+//   console.log(desc);
+// }
+
+// function Log3(target: object, key: string, desc: PropertyDescriptor) {
+//   console.log("Method decorator");
+//   console.log(target);
+//   console.log(key);
+//   console.log(desc);
+// }
+
+// function Log4(target: object, key: string, position: number) {
+//   console.log("Paramater decorator");
+//   console.log(target);
+//   console.log(key);
+//   console.log(position);
+// }
+
+// class Product {
+//   @Log
+//   title: string;
+//   private _price: number;
+
+//   @Log2
+//   set price(val: number) {
+//     if (val > 0) {
+//       this._price = val;
+//     } else {
+//       throw new Error("Invalid price - should be positive!");
+//     }
+//   }
+
+//   constructor(t: string, p: number) {
+//     this.title = t;
+//     this._price = p;
+//   }
+
+//   @Log3
+//   getPriceWithTax(@Log4 tax: number) {
+//     return this._price * (1 + tax);
+//   }
+// }
+
+// const book1 = new Product("book1", 50);
+// const book2 = new Product("book2", 10 0);
