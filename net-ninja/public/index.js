471c428f1c13e4d8358120310e73a9889fc2f4fd
Nozomi Ishii <non.ankt@gmail.com>
2020-05-19 18:36:22 +0900

    typescript class

--- net-ninja/public/index.js
+++ net-ninja/public/index.js
@@ -1,16 +1,27 @@
 "use strict";
-var greet;
-greet = function () {
-    console.log("hello, again");
-};
-var add = function (a, b, c) {
-    if (c === void 0) { c = 10; }
-    console.log(a + b);
-    console.log(c);
-};
-add(5, 20);
-var minus = function (a, b) {
-    return a - b;
-};
-var result = minus(560, 5);
-console.log(result);
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes " + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice("mario", "work on mario website", 250);
+var Invoices = [];
+var ancher = document.querySelector("a");
+console.log(ancher.href);
+var form = document.querySelector("form");
+// const form = document.querySelector('form')
+var btn = document.querySelector("form-btn");
+var type = document.querySelector("#type");
+var tofrom = document.querySelector("#tofrom");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
--- net-ninja/src/index.ts
+++ net-ninja/src/index.ts
@@ -1,8 +1,25 @@
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+  format() {
+    return `${this.client} owes ${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice("mario", "work on mario website", 250);
+let Invoices: Invoice[] = [];
+
 const ancher = document.querySelector("a")!;
 
 console.log(ancher.href);
 
-const form = document.getElementById("form-btn") as HTMLFontElement;
+const form = document.querySelector("form") as HTMLFormElement;
 // const form = document.querySelector('form')
 
 const btn = document.querySelector("form-btn") as HTMLButtonElement;
