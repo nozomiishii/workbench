94309f8f6992f9fcae0113ed3da8b7b8062040c2
Nozomi Ishii <non.ankt@gmail.com>
2020-05-20 10:04:09 +0900

    react

--- richard/src/App.tsx
+++ richard/src/App.tsx
@@ -1,71 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
-interface Todo {
-  text: string;
-  complete: boolean;
-}
-
-function App(): JSX.Element {
-  const [value, setValue] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
-    e.preventDefault();
-    addTodo(value);
-    setValue("");
-  };
-
-  const addTodo = (text: string): void => {
-    const newTodo: Todo[] = [...todos, { text, complete: false }];
-    setTodos(newTodo);
-  };
-
-  const completeTodo = (index: number): void => {
-    const newTodos: Todo[] = [...todos];
-    newTodos[index].complete = !newTodos[index].complete;
-    setTodos(newTodos);
-  };
-
-  const removeTodo = (id: number) => {
-    setTodos((prev: Todo[]) => {
-      return prev.filter((todo: Todo, index: number) => {
-        console.log("index", index);
-        return index !== id;
-      });
-    });
-  };
-  return (
-    <>
-      <h1>todo list</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          required
-        />
-        <button type="submit">add todo</button>
-      </form>
-      <section>
-        {todos.map((todo: Todo, index: number) => (
-          <div key={index}>
-            <div
-              style={{
-                textDecoration: todo.complete ? "line-through" : "none",
-              }}
-            >
-              {todo.text}
-            </div>
-            <button type="button" onClick={() => completeTodo(index)}>
-              {todo.complete ? "complete" : "inComplete"}
-            </button>
-            <button type="button" onClick={() => removeTodo(index)}>
-              remove
-            </button>
-          </div>
-        ))}
-      </section>
-    </>
-  );
-}
+const App = () => {
+  return <div></div>;
+};
 
 export default App;
