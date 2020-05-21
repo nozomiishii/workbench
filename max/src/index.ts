52800e13201b45674dae2641a219a2f60f524e2e
Nozomi Ishii <non.ankt@gmail.com>
2020-05-22 02:20:37 +0900

    geocode with typescript

--- max/src/index.ts
+++ max/src/index.ts
@@ -1,6 +1,23 @@
+import axios from "axios";
 const form = document.querySelector("form") as HTMLFormElement;
+const addressInput = document.getElementById("address") as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyBNLrJhOMz6idD05pzfn5lhA-TAw-mAZCU";
 
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
   console.log("object");
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}?key=${GOOGLE_API_KEY}`
+    )
+    .then((response: any) => {
+      const coordinates = response;
+      console.log(coordinates);
+    })
+    .catch((err: any) => console.error(err));
 });
