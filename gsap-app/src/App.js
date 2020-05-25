79c71c64e7f79298dfaa9283ba38ae6a204e08d1
Nozomi Ishii <non.ankt@gmail.com>
2020-05-25 17:45:51 +0900

    study jest

---
+++ gsap-app/src/App.js
@@ -0,0 +1,28 @@
+import React, { useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { TweenMax, Power3 } from "gsap";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
