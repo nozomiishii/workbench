0a61fc34cfeedb24f812b73a234842b5d2998268
Nozomi Ishii <non.ankt@gmail.com>
2020-06-02 20:59:53 +0900

    sudoku app

--- sudoku/src/index.tsx
+++ sudoku/src/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { unregister } from "./core";
+import { GlobalStyles } from "./styles";
+
+const txt = "test";
+console.log("txt", txt);
+console.log("txt", txt);
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>hello world</div>
+    <>
+      <GlobalStyles />
+      <div>hello world</div>
+    </>
   </React.StrictMode>,
   document.getElementById("root")
 );
