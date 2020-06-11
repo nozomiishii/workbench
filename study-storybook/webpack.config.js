ad841cb4b2f636b79a98cc0eb89db849a05bd541
Nozomi Ishii <non.ankt@gmail.com>
2020-06-11 17:48:43 +0900

    storybook

---
+++ study-storybook/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "custom-button.js",
+    library: "custom-button",
+    lib,
+  },
+};
