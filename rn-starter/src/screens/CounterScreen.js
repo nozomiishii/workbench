468ccfb7f86775614e623871c9fcc646a01a90fe
Nozomi Ishii <non.ankt@gmail.com>
2020-06-22 20:08:31 +0900

    counter excercise

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useReducer } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+const initialState = {
+  count: 0,
+};
+
+const COUNT = 1;
+
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case 'PULS':
+      return {
+        ...state,
+        count: state.count + action.payload,
+      };
+    case 'MINUS':
+      return {
+        ...state,
+        count: state.count - action.payload,
+      };
+    default:
+      return state;
+  }
+};
 const CounterScreen = () => {
+  const [state, dispatch] = useReducer(counterReducer, initialState);
+  const { count } = state;
   return (
     <View>
-      <Text>counter</Text>
+      <Text>counter: {count}</Text>
+      <Button
+        title='+'
+        onPress={() => dispatch({ type: 'PULS', payload: COUNT })}
+      />
+      <Button
+        title='-'
+        onPress={() => dispatch({ type: 'MINUS', payload: COUNT })}
+      />
     </View>
   );
 };
