3b4b9f54c5ceb92b7bc3d5b62c1e8a3cbd4b90c6
Nozomi Ishii <non.ankt@gmail.com>
2020-06-29 00:51:29 +0900

    auth

--- albums/App.js
+++ albums/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Text} from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => {
   return (
     <>
-      <Header />
+      <Header headerText="Albums " />
+      <AlbumList />
       <Text>Some text</Text>
     </>
   );
--- albums/src/components/Header.js
+++ albums/src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-const Header = () => {
+const Header = (props) => {
   const {textStyle, viewStyle} = styles;
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>Albums</Text>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
@@ -17,8 +17,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#F8F8F8',
     justifyContent: 'center',
     alignItems: 'center',
-    height: 60,
+    height: 100,
     paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
   },
   textStyle: {
     fontSize: 20,
