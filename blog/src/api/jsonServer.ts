b6a1601d0c4edc28d9674362b3fbaae060eea514
Nozomi Ishii <non.ankt@gmail.com>
2020-07-03 20:19:53 +0900

    clean up

--- blog/src/api/jsonServer.ts
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'http://672313954684.ngrok.io',
-});
--- marioplan/src/store/reducers/authReducer.js
+++
@@ -1,4 +0,0 @@
-const initState = {};
-export const authReducer = (state = initState, action) => {
-  return state;
-};
