263302d3eff4bfa95ed18762b48ad55d8cd65413
Nozomi Ishii <non.ankt@gmail.com>
2020-12-05 20:31:48 +0900

    next js

---
+++ src/pages/pageTwo.js
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>page 2</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main className='min-h-screen flex flex-col justify-center'>
+        <Image
+          className='m-auto my-4 w-24 sm:w-auto'
+          src='/assets/logo.png'
+          alt='logo'
+          width={500}
+          height={500}
+        />
+        <h1 className='mx-auto text-4xl font-bold'>page2!</h1>
+        <Link href='/'>
+          <a>Go home</a>
+        </Link>
+      </main>
+    </div>
+  );
+}
