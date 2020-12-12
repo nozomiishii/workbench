7f0a28c9c7b6d998be12682919d6e0c50729dd84
Nozomi Ishii <non.ankt@gmail.com>
2020-12-12 21:59:33 +0900

    reset

--- utils/__test__/slugify.test.js
+++
@@ -1,6 +0,0 @@
-import { slugify } from '../slugify';
-
-test('converts a string to a slug', () => {
-  expect(slugify('Tab 1')).toBe('tab-1');
-  expect(slugify("Amy's Tab")).toBe('amys-tab');
-});
