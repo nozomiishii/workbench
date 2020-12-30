562215adb6aeb4d950a334e6358a748ccca9dbbb
Nozomi Ishii <non.ankt@gmail.com>
2020-12-30 15:19:15 +0900

    reset

--- graphql-ninja/server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import express, { Application, Request, Response } from 'express';
-
-const app: Application = express();
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('hello');
-});
-
-app.listen(3000, () => console.log('listening on port (http://localhost:3000/)'));
