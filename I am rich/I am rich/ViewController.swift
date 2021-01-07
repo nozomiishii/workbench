5575858a0c0965f43cbf108e37975b5a21ff3910
Nozomi Ishii <non.ankt@gmail.com>
2021-01-07 10:36:09 +0900

    swift

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Nozomi Ishii on 6/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
